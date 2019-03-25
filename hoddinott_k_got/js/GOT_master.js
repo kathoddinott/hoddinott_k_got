c398c679af92b2cfc3ce83b6e122cf72face7b16
kathoddinott <43182997+kathoddinott@users.noreply.github.com>
2019-03-25 15:19:23 -0400

    Add files via upload

--- hoddinott_k_got/js/GOT_master.js
+++ hoddinott_k_got/js/GOT_master.js
@@ -3,17 +3,50 @@
 
 	const 	shields		= document.querySelectorAll('.sigil-container'),
 			lightBox 	= document.querySelector('.lightbox'),
-			video		= document.querySelector('video');
+			video		= document.querySelector('video'),
+			closeLB 	= document.querySelector('.lightbox-close'),
+			banners		= document.querySelector('#houseImages'),
+			houseName 	= document.querySelector('.house-name'),
+			houseInfo	= document.querySelector('.house-info');
+
+	const houseData 	= [
+		`House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`
+		];
 
 	function showLightbox() {
-		lightBox.classList.add('show-lightbox');
+		let targetHouse = this.className.split(" ")[1];
+
+		let targetSrc = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		houseName.textContent = `House ${targetSrc}`;
+
+		houseInfo.textContent = houseData[0];
+
+		video.src = `video/House-${targetSrc}.mp4`;
+
+		lightbox.classlist.add('lightbox-on');
+
+		video.load();
+		video.play();
 	}
 
 	function hideLightbox() {
-		lightBox.classList.remove('show-lightbox');
+		lightBox.classList.remove('lightbox-on');
+
+		video.currentTime = 0;
+		video.pause();
 	}
 
-	shields.forEach(shield => shield.addEventListener('click', showLightbox));
+	function animateBanner() {
+		const offSet = 600;
 
-	video.addEventListener('ended', hideLightbox);
+		totalOffset = this.dataset.offset = offSet;
+
+		TweenMax.to(banners, 0.8, { right: totalOffset });
+	}
+
+	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+
+	video.addEventListener('ended', closeLightbox);
+	closeLB.addEventListener('click', closeLightbox);
 })();
